f3dc15e9c77bda30f2c3f62f0c5a368bb6ef07dd
giulia-denapoli <81319169+giulia-dn@users.noreply.github.com>
2024-02-20 19:39:20 +0100

    remove useless comment Ricerca.java

--- demo - Copia/src/main/java/com/example/demo/controller/Ricerca.java
+++ demo - Copia/src/main/java/com/example/demo/controller/Ricerca.java
@@ -32,10 +32,8 @@ public class Ricerca implements Observer {
     private Label id_member;
     @FXML
     private TextArea risultatoCarrello;
-    private Cart cart = new Cart();//private com.example.demo.model.Cart cart = com.example.demo.model.Cart.getInstance();
-    //private com.example.demo.model.Product product=com.example.demo.model.Book.getInstance(0,"", "");
-    private  Product product ;//private Product book = Book.getInstance(0,"", "");
-    //private Product ebook = Ebook.getInstance(0,"", "");
+    private Cart cart = new Cart();
+    private  Product product ;
     private ProductDaoConcrete pdc = new ProductDaoConcrete();
     private List<Product> products = new ArrayList<>();
     public void ricercaTitolo(ActionEvent actionEvent) {
@@ -85,21 +83,20 @@ public class Ricerca implements Observer {
         for(Product product: products1){
             result += product.showInfo();
         }
-        risultatoCarrello.setText(result);//risultatoCarrello.setText(cart.showCart());
+        risultatoCarrello.setText(result);
         int value = cart.getTotal();
         contatore.setText(String.valueOf(value));
 
     }
     @Override
     public void update() {
-        //cart = (com.example.demo.model.Cart) o;
         infoProdotto.setVisible(true);
         infoProdotto.setText("Prodotto aggiunto correttamente");
         String result="";
         for(Product product: cart.getProducts()){
             result += product.showInfo();
         }
-        prodottiAggiunti.setText(result);//prodottiAggiunti.setText(cart.allProduct());
+        prodottiAggiunti.setText(result);
         mostraCarrello();
     }
 
@@ -108,86 +105,3 @@ public class Ricerca implements Observer {
 
 }
 
-/*public class Ricerca implements Observer {
-    @FXML
-    private TextField testoTitolo;
-    @FXML
-    private TextField testoAutore;
-    @FXML
-    private TextArea risultatiRicerca;
-    @FXML
-    private TextField contatore;
-    @FXML
-    private TextField id;
-    @FXML
-    private Label infoProdotto;
-    @FXML
-    private TextArea prodottiAggiunti;
-    @FXML
-    private Label id_member;
-    @FXML
-    private TextArea risultatoCarrello;
-    private Cart cart = Cart.getInstance();
-    //private Product product=Book.getInstance(0,"", "");
-    private Product book = Book.getInstance(0,"", "");
-    private Product ebook = Ebook.getInstance(0,"", "");
-    public void ricercaTitolo(ActionEvent actionEvent) {
-        testoAutore.setVisible(false);
-        testoAutore.setText("");
-        testoTitolo.setVisible(true);
-    }
-    public void ricercaAutore(ActionEvent actionEvent) {
-        testoTitolo.setVisible(false);
-        testoTitolo.setText("");
-        testoAutore.setVisible(true);
-    }
-    public void ricerca(ActionEvent actionEvent) {
-        String ricerca = "";
-        if(testoTitolo.isVisible()) {
-            ricerca = cart.searchTitle(testoTitolo.getText());
-        }
-        else {
-            ricerca = cart.searchAuthor(testoAutore.getText());
-        }
-        risultatiRicerca.setText(ricerca);
-    }
-
-    public void addCart(){
-        cart.subscribe(this);
-        int value = Integer.parseInt(id.getText());
-        //product = searchId_product(value);
-        String p = cart.searchType_product(value);
-        if(p.equals("book")){
-            book=book.searchProduct(value);
-            cart.add(book);
-        }else if(p.equals("ebook")){
-            ebook=ebook.searchProduct(value);
-            cart.add(ebook);
-        }else {
-            infoProdotto.setVisible(true);
-            infoProdotto.setText("Prodotto non disponibile");
-        }
-    }
-    public void mostraCarrello() {
-        id_member.setVisible(true);
-        id_member.setText(Login.getStudent().getId_member());
-        risultatoCarrello.setVisible(true);
-        risultatoCarrello.setText(cart.showCart());
-        int value = cart.getTotal();
-        contatore.setText(String.valueOf(value));
-
-    }
-    @Override
-    public void update() {
-        //cart = (Cart) o;
-        infoProdotto.setVisible(true);
-        infoProdotto.setText("Prodotto aggiunto correttamente");
-        prodottiAggiunti.setText(cart.allProduct());
-        mostraCarrello();
-    }
-
-
-
-
-}
-*/
