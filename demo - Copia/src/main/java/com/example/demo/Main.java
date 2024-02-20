e0d49d91aa625b1da014dda0240276970d7be092
giulia-denapoli <81319169+giulia-dn@users.noreply.github.com>
2024-02-20 19:44:00 +0100

    remove useless comment Update Main.java

--- demo - Copia/src/main/java/com/example/demo/Main.java
+++ demo - Copia/src/main/java/com/example/demo/Main.java
@@ -20,41 +20,5 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
-/*
-    public static void main(String[] args) {
-        String r = searchAuthor("Edgar Allan Poe");
-        System.out.println("___________________________________________________________");
-        System.out.println(r);
-        launch(args);
-    }
 
-    public static String searchAuthor(String author) {
-        Connection cn;
-        Statement st;
-        ResultSet rs;
-        String sql;
-        String result = "";
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("ClassNotFoundException :");
-            System.err.println(e.getMessage());
-        }
-        try {//crea connessione al database
-            cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/libray2", "root", "");
-            sql = "SELECT id_product, title, author, product, available FROM product WHERE author='" + author + "';";
-            st = cn.createStatement();
-            rs = st.executeQuery(sql);
-            while (rs.next()) {
-                System.out.println("id_product=" + rs.getString("id_product") + ", product='" + rs.getString("product") + "', title='" + rs.getString("title") + "', author='" + rs.getString("author") + "', available=" + rs.getString("available"));
-                result = result + "id_product=" + rs.getString("id_product") + ", product='" + rs.getString("product") + "', title='" + rs.getString("title") + "', author='" + rs.getString("author") + "', available=" + rs.getString("available") + "\n";
-            }
-            cn.close();
-        } catch (SQLException e) {
-            System.out.println("errore:" + e.getMessage());
-        }
-        return result;
-    }
-
- */
 }
